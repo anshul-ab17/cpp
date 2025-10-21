a0c0701964935af5675b75591a12f23ddd0eedc1
Anshul Bharat <anshul.ab17x@gmail.com>
2025-10-21 10:30:00 +0530

    feat(prefix-sum): range sum query immutable

---
+++ leetcode/prefix-sum/0303_range_sum_query.cpp
@@ -0,0 +1,16 @@
+// LC 303. Range Sum Query - Immutable | Easy
+#include <vector>
+using namespace std;
+
+class NumArray {
+    vector<int> prefix;
+public:
+    NumArray(vector<int>& nums) {
+        prefix.resize(nums.size() + 1, 0);
+        for (int i = 0; i < nums.size(); i++)
+            prefix[i + 1] = prefix[i] + nums[i];
+    }
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
