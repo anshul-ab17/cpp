2c909bd55e765ae85d307343567a110f0ff7e96a
Anshul Bharat <anshul.ab17x@gmail.com>
2025-11-19 14:00:00 +0530

    feat(binary-search): search insert position

---
+++ leetcode/binary-search/0035_search_insert_position.py
@@ -0,0 +1,10 @@
+# LC 35. Search Insert Position | Easy
+class Solution:
+    def searchInsert(self, nums, target):
+        l, r = 0, len(nums) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target: return m
+            elif nums[m] < target: l = m + 1
+            else: r = m - 1
+        return l
