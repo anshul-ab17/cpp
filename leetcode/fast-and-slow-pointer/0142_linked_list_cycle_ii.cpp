3bb04c7e1cf2544600821e515bb37eeb512527ac
Anshul Bharat <anshul.ab17x@gmail.com>
2025-12-21 14:00:00 +0530

    feat(fast-slow): linked list cycle ii - find entry

---
+++ leetcode/fast-and-slow-pointer/0142_linked_list_cycle_ii.cpp
@@ -0,0 +1,18 @@
+// LC 142. Linked List Cycle II | Medium
+struct ListNode { int val; ListNode* next; ListNode(int x) : val(x), next(nullptr) {} };
+
+class Solution {
+public:
+    ListNode* detectCycle(ListNode* head) {
+        ListNode *slow = head, *fast = head;
+        while (fast && fast->next) {
+            slow = slow->next; fast = fast->next->next;
+            if (slow == fast) {
+                slow = head;
+                while (slow != fast) { slow = slow->next; fast = fast->next; }
+                return slow;
+            }
+        }
+        return nullptr;
+    }
+};
