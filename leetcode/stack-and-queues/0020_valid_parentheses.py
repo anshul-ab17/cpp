674e36fcc1c45873309a35eda250239aa7deff78
Anshul Bharat <anshul.ab17x@gmail.com>
2025-12-29 15:00:00 +0530

    feat(stack): valid parentheses

---
+++ leetcode/stack-and-queues/0020_valid_parentheses.py
@@ -0,0 +1,11 @@
+# LC 20. Valid Parentheses | Easy
+class Solution:
+    def isValid(self, s):
+        stack = []
+        pairs = {')':'(', ']':'[', '}':'{'}
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack[-1] != pairs[ch]: return False
+                stack.pop()
+            else: stack.append(ch)
+        return not stack
