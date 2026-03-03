85f78212ac50d42d996ec94d915ceaf9f325b1ac
Anshul Bharat <anshul.ab17x@gmail.com>
2026-03-03 14:00:00 +0530

    feat(heap): find median from data stream

---
+++ leetcode/heap/0295_find_median_data_stream.cpp
@@ -0,0 +1,16 @@
+// LC 295. Find Median from Data Stream | Hard
+#include <queue>
+using namespace std;
+
+class MedianFinder {
+    priority_queue<int> lo;
+    priority_queue<int, vector<int>, greater<int>> hi;
+public:
+    void addNum(int num) {
+        lo.push(num); hi.push(lo.top()); lo.pop();
+        if (hi.size() > lo.size()) { lo.push(hi.top()); hi.pop(); }
+    }
+    double findMedian() {
+        return lo.size() > hi.size() ? lo.top() : (lo.top() + hi.top()) / 2.0;
+    }
+};
