6242de38937fb02e01ee08e7bfcb4bfd5401a90b
Anshul Bharat <anshul.ab17x@gmail.com>
2026-04-07 15:00:00 +0530

    feat(dp/1d): word break

---
+++ leetcode/dp/1d/0139_word_break.py
@@ -0,0 +1,8 @@
+# LC 139. Word Break | Medium
+class Solution:
+    def wordBreak(self, s, wordDict):
+        ws = set(wordDict); n = len(s); dp = [False]*(n+1); dp[0] = True
+        for i in range(1, n+1):
+            for j in range(i):
+                if dp[j] and s[j:i] in ws: dp[i] = True; break
+        return dp[n]
